21499368780c7488ede61c17b3910841c6bae540
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-29 22:59:01 +0200

    Updated the Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import PropTypes from "prop-types"
+import Button from "./Button"
 
-const Header = ({title}) => {
-  return (
-    <header className="header">
-        <h1>{title}</h1>
-    </header>
-  )
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log("Click");
+    }
+
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <Button text="Add" onClick={onClick} />
+        </header>
+    )
 
 }
 
 Header.defaultProps = {
-  title: "Task Tracker",
+    title: "Task Tracker",
 }
 
 Header.propTypes = {
