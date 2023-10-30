bbf4e992cd980b1478fb45c2065aba785f12e65a
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-30 12:54:22 +0200

    Imported the TaskReducer to the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
+import taskReducer from "../slices/taskSlice"
 
 export const store = configureStore({
-    reducer: {},
+    reducer: taskReducer,
 })
