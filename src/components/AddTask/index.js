150b834ae5c6a029e5c91c67a522536081ff2b03
rasha-massoud <rashamassoud1998@gmail.com>
2023-10-30 13:46:36 +0200

    Added the AddTask Component

---
+++ src/components/AddTask/index.js
@@ -0,0 +1,74 @@
+import { useState } from "react"
+
+const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const TextChanged = (e) => {
+        setText(e.target.value)
+    }
+
+    const DayChanged = (e) => {
+        setDay(e.target.value)
+    }
+
+    const ReminderChecked = (e) => {
+        setReminder(e.currentTarget.checked)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert('Please add a task')
+            return
+        }
+        else if (!day) {
+            alert('Please add a day and time')
+            return
+        }
+
+        // Add Data to the store
+
+        setText("")
+        setDay("")
+        setReminder("")
+    }
+
+    return (
+        <form className="add-form" onSubmit={onSubmit}>
+            <div className="form-control">
+                <label>Task</label>
+                <input
+                    type="text"
+                    placeholder="Add Task"
+                    value={text}
+                    onChange={TextChanged}
+                />
+            </div>
+            <div className="form-control">
+                <label>Day & Time</label>
+                <input
+                    type="text"
+                    placeholder="Add Day & Time"
+                    value={day}
+                    onChange={DayChanged}
+                />
+            </div>
+            <div className="form-control">
+                <label>Set Reminder</label>
+                <input
+                    type="checkbox"
+                    checked={reminder}
+                    value={reminder}
+                    onChange={ReminderChecked}
+                />
+            </div>
+
+            <input type="submit" value="Save Task" className="btn btn-block"/>
+        </form>
+    )
+}
+
+export default AddTask;
